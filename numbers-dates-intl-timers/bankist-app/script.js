fe6ce0d1ef0599417b441c359256dc3556f53bee
Michele Cheow <ldnallen@gmail.com>
2023-06-19 13:45:22 -0700

    changed user name

--- numbers-dates-intl-timers/bankist-app/script.js
+++ numbers-dates-intl-timers/bankist-app/script.js
@@ -8,7 +8,7 @@
 // Data - Contains movement dates, currency and locale
 
 const account1 = {
-  owner: "Jonas Schmedtmann",
+  owner: "Michele Cheow",
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
