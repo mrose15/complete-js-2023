4cead6e1b65da8062fe0db842c1db05c01504942
Michele Cheow <ldnallen@gmail.com>
2023-07-25 08:05:15 -0700

    removed button focus outline none, created full bleed class, added scrollIntoView method

--- advanced-dom-events/script.js
+++ advanced-dom-events/script.js
@@ -35,28 +35,24 @@ document.addEventListener("keydown", function (e) {
 ///////////////////////////////////////
 
 /* Selecting, Creating, Deleting, Elements */
-console.log(document.documentElement);
+/* console.log(document.documentElement);
 console.log(document.head);
-console.log(document.body);
+console.log(document.body); */
 
 const header = document.querySelector(".header");
 
 // returns a node list, does not update on DOM change
 const allSections = document.querySelectorAll(".section");
-console.log(allSections);
 
 document.getElementById("section--1");
 
 // returns html collection, updates when the dom updates (ie: element is deleted)
 const buttons = document.getElementsByTagName("button");
-console.log(buttons);
-
-console.log(document.getElementsByClassName("btn"));
 
 // creating and inserting elements
 //create DOM object
 const message = document.createElement("div");
-message.classList.add("cookie-message");
+message.classList.add("cookie-message", "full-bleed");
 //message.textContent = 'We use cookies for improved functionality and analytics.'
 message.innerHTML =
   "We use cookies for improved functionality and analytics. <button class='btn btn--close-cookie'>Got It!</button>";
@@ -78,42 +74,19 @@ document
   });
 
 // Styles
-message.style.backgroundColor = "#37383d";
-message.style.width = "120%";
-
-console.log(message.style.color);
-console.log(message.style.backgroundColor);
-
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
-
 message.style.height =
   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
 
-document.documentElement.style.setProperty("--color-primary", "purple");
+//document.documentElement.style.setProperty("--color-primary", "purple");
 
 //Attributes
 const logo = document.querySelector(".nav__logo");
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
-
 logo.alt = "Beautiful minimalist logo";
 
 //non-standard
-console.log(logo.designer);
-console.log(logo.getAttribute("designer"));
 logo.setAttribute("company", "Bankist");
 
-console.log(logo.src);
-console.log(logo.getAttribute("src"));
-
 const link = document.querySelector(".nav__link--btn");
-console.log(link.href);
-console.log(link.getAttribute("href"));
-
-// data attributes
-console.log(logo.dataset.versionNumber);
 
 // classes
 logo.classList.add("c", "j");
@@ -123,3 +96,29 @@ logo.classList.contains("c"); // not includes like it is in arrays
 
 // don't use b/c will override current classes
 logo.className = "jonas";
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  //console.log(s1coords);
+
+  //console.log(`button element`);
+  //console.log(e.target.getBoundingClientRect());
+
+  //console.log("Current scroll (X/Y)", window.scrollX, window.scrollY);
+
+  //console.log(s1coords.top + window.scrollY);
+
+  //Scrolling
+  //old shool way
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: "smooth",
+  // });
+
+  // new way
+  section1.scrollIntoView({ behavior: "smooth" });
+});
