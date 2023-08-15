5a18df78b9c15997ff07ede5fd757a9b7e88b0b8
Michele Cheow <ldnallen@gmail.com>
2023-08-15 13:09:26 -0700

    Lazy loading images

--- advanced-dom-events/script.js
+++ advanced-dom-events/script.js
@@ -1,5 +1,9 @@
 "use strict";
-//TODO: add hamburger menu, make page responsive
+/*TODO: 
+- add hamburger menu, make page responsive
+- conditional to test is user just reloaded in the middle of the page for sections
+- conditional to test is user just reloaded in the middle of the page for lazy loading of images in section 1
+*/
 
 ///////////////////////////////////////
 // Modal window
@@ -177,7 +181,6 @@ const allSections = document.querySelectorAll(".section");
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove("section--hidden");
@@ -190,11 +193,36 @@ const sectionObserver = new IntersectionObserver(revealSection, {
   threshold: 0.15,
 });
 
-//todo: write conditional to test is user just reloaded in the middle of the page
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  // replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  //need to wait for page to finish loading
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
 
 ///////////////////////////////////////
 ///////////////////////////////////////
