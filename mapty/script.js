855cd0a3ddc9c188b79672658fd2d903bdadacf2
Michele Cheow <ldnallen@gmail.com>
2024-01-04 13:53:35 -0800

    added click event

--- mapty/script.js
+++ mapty/script.js
@@ -30,6 +30,24 @@ if (navigator.geolocation)
         .addTo(map)
         .bindPopup("A pretty CSS popup.<br> Easily customizable.")
         .openPopup();
+
+      map.on("click", function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("Workout")
+          .openPopup();
+      });
     },
     function () {
       alert("Could not get your position");
