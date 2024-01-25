6a04929b4cd70abdf8c14760820d6f7ad96e6c40
Michele Cheow <ldnallen@gmail.com>
2024-01-24 19:03:35 -0800

    added async-js files, wrote API call, used for in to loop through currencies

---
+++ async-js/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const currency = () => {
+      for (const currencyCode in data.currencies) {
+        if (data.currencies.hasOwn(currencyCode)) {
+          const currencyInfo = data.currencies[currencyCode];
+          return `<p class="country__row"><span>💰</span>${currencyInfo.name}</p>`;
+        }
+      }
+    };
+
+    const html = `<article class="country">
+  <img class="country__img" src="${data.flags.svg}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} million people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+    ${currency()}
+  </div>
+</article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
