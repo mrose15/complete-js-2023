0da49d1e87c2ee7b7dc8309bf64cbef55cad5a18
Michele Cheow <ldnallen@gmail.com>
2024-02-14 22:15:14 -0500

    converted whereamI promise (then/catch) to async IIFE

--- async-js/script.js
+++ async-js/script.js
@@ -299,7 +299,17 @@ console.log('1: Will get location');
 
 // const city = whereAmI();
 // console.log(city);
-whereAmI()
-  .then(city => console.log(`2: ${city}`))
-  .catch(err => console.error(`2: ${err.message} 💣 `))
-  .finally(() => console.log('3: Finished Getting location'));
+// whereAmI()
+//   .then(city => console.log(`2: ${city}`))
+//   .catch(err => console.error(`2: ${err.message} 💣 `))
+//   .finally(() => console.log('3: Finished Getting location'));
+
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: ${err.message} 💣 `);
+  }
+  console.log('3: Finished Getting location');
+})();
