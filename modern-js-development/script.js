eeb6a163570c3974236b620c37cd67321b1ef218
Michele Cheow <ldnallen@gmail.com>
2024-02-19 23:23:38 -0500

    added imports to scripts and renamed imports using ‘as’ keyword

--- modern-js-development/script.js
+++ modern-js-development/script.js
@@ -1,4 +1,11 @@
 // Importing module
-import "./shoppingCart.js";
+import {
+  addToCart,
+  totalPrice as price,
+  totalQuantity as qty,
+} from "./shoppingCart.js";
 
 console.log("Importing module");
+
+addToCart("bread", 5);
+console.log(price, qty);
