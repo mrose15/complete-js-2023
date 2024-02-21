e9c9cf4b1ef10cd8c98a169b6114aa752638661e
Michele Cheow <ldnallen@gmail.com>
2024-02-20 21:50:34 -0500

    use top level await to consume lastPost with then method

--- modern-js-development/script.js
+++ modern-js-development/script.js
@@ -25,11 +25,28 @@ add("apples", 2);
 console.log(cart);
 
 //can new use await keyword outside of an async function, or top level await, only works in modules
-console.log("Start fetching");
-const res = await fetch("https://jsonplaceholder.typicode.com/users/1/posts");
-const data = await res.json();
-console.log(data);
+// console.log("Start fetching");
+// const res = await fetch("https://jsonplaceholder.typicode.com/users/1/posts");
+// const data = await res.json();
+// console.log(data);
 
-//but this will block the execution of the entire module, which is not great
+//but this will block the execution of the entire module, which is not great if it's a really long running task so use with caution
+// console.log("Something");
 
-console.log("Something");
+//real world example
+const getLastPost = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/1/posts");
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = getLastPost();
+console.log(lastPost); //calling an async function returns a promise and not the data; by the time we run this line of code, the data hasn't arrived, so you'll see a pending promise.
+
+// so instead use regular Promises but it's not very clean
+//lastPost.then((last) => console.log(last));
+
+// we can use top level await for this
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
