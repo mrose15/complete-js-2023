ea9eb8dd65d1ca3c52638345029953f623c9d4ea
Michele Cheow <ldnallen@gmail.com>
2024-02-20 21:55:36 -0500

    test top level await in exporting module to show that it is blocking code in importing module

--- modern-js-development/shoppingCart.js
+++ modern-js-development/shoppingCart.js
@@ -1,6 +1,12 @@
 // Exporting module, gets executed first
 console.log("Exporting module");
 
+//Blocking code, code in script.js has to wait for this, which is bad for performance
+// using top level await will block this from running and will keep the code in script.js from running until this is finished
+// console.log("Start fetching users");
+// const res = await fetch("https://jsonplaceholder.typicode.com/users/1/users");
+// console.log("Finish fetching users");
+
 const shippingCost = 10;
 export const cart = [];
 
