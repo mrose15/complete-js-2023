bba6967026635631a56bbc70861962c5fd9566b7
Michele Cheow <ldnallen@gmail.com>
2024-03-09 21:30:56 -0800

    used optional chaining and nullish coalescing operator again to set const limit for checkExpenses function, changed el to entry

--- modern-js-development/clean.js
+++ modern-js-development/clean.js
@@ -30,16 +30,11 @@ addExpense(200, "Stuff", "Jay");
 console.log(budget);
 
 const checkExpenses = function () {
-  for (const el of budget) {
-    let lim;
-    if (spendingLimits[el.user]) {
-      lim = spendingLimits[el.user];
-    } else {
-      lim = 0;
-    }
+  for (const entry of budget) {
+    const limit = spendingLimits?.[entry.user] ?? 0;
 
-    if (el.value < -lim) {
-      el.flag = "limit";
+    if (entry.value < -limit) {
+      entry.flag = "limit";
     }
   }
 };
