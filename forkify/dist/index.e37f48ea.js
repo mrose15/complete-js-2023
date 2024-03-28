ebb02257a46386a55ce687f12e9ef464265569b9
Michele Cheow <ldnallen@gmail.com>
2024-03-28 09:44:52 -0700

    listening for load and hashchange events

--- forkify/dist/index.e37f48ea.js
+++ forkify/dist/index.e37f48ea.js
@@ -612,10 +612,11 @@ const renderSpinner = function(parentEl) {
 };
 const showRecipe = async function() {
     try {
+        const id = window.location.hash.slice(1);
+        if (!id) return;
         // 1) loading recipe
         renderSpinner(recipeContainer);
-        const res = await fetch(//'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca5d'
-        "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
         const data = await res.json();
         if (!res.ok) throw new Error(`${data.message} (${res.status})`);
         console.log(res, data);
@@ -725,7 +726,10 @@ const showRecipe = async function() {
         alert(err);
     }
 };
-showRecipe();
+[
+    "hashchange",
+    "load"
+].forEach((event)=>window.addEventListener(event, showRecipe));
 
 },{"url:../img/icons.svg":"loVOp","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","core-js/modules/web.immediate.js":"49tUX","regenerator-runtime/runtime":"dXNgZ"}],"loVOp":[function(require,module,exports) {
 module.exports = require("9bcc84ee5d265e38").getBundleURL("hWUTQ") + "icons.dfd7a6db.svg" + "?" + Date.now();
